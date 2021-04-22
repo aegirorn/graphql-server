2140d61338afadc0f3f62bc4e2b4511e1098120f
Paula Nerenberg <paulita.glam@gmail.com>
2021-04-22 20:41:11 +0000

    first commit

---
+++ resolvers/Animal.js
@@ -0,0 +1,15 @@
+
+
+
+
+const Animal = {
+
+    category: (parent, args, { categories }) => {
+        return categories.find((category) => {
+          return category.id === parent.category;
+        })
+      }
+
+}
+
+module.exports = Animal
